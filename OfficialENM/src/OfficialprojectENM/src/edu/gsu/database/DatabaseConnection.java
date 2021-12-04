fbd807da80d173d6d9814a432bccaa9970cf8669
nicolekim <nicolekim@192.168.68.91>
2021-12-03 20:21:16 -0500

    database

---
+++ OfficialENM/src/OfficialprojectENM/src/edu/gsu/database/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package edu.gsu.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	
+	public static void main(String[] args) throws Exception {
+		
+		getConnection();
+		
+	}
+	
+	public static Connection getConnection() throws Exception{
+		try {
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://localhost:3306/CIS3270project";
+			String username = "root";
+			String password = "Ek96046!!!";
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url,username,password);
+			System.out.println("Connected");
+			return conn;
+		}catch(Exception e) {System.out.println(e);}
+		
+		return null;
+	}
+
+}
